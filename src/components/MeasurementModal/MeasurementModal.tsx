f1b06a413f1c6e0ea85ff4a42b9c9b12c4414794
VladislavVanyushkin <vlad.vanyushkin.96@mail.ru>
2024-12-28 18:14:53 +0300

    feat: added validation to select field to create measurement form

--- src/components/MeasurementModal/MeasurementModal.tsx
+++ src/components/MeasurementModal/MeasurementModal.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Fade,
+  FormControl,
   MenuItem,
   Modal,
   TextField,
@@ -19,9 +20,21 @@ interface MeasurementModal {
   handleClose: () => void;
 }
 
+// Сделать потом
+// interface Meal {
+//   portion: number;
+//   dish: string;
+// }
+
+// interface AfterMealMeasurement {
+//   meal: Meal[];
+// }
+
 interface FormTypes {
   typeOfMeasurement: string;
-  food?: string;
+  // afterMealMeasurement?: AfterMealMeasurement; // Сделать потом
+  food?: string; // Временно так
+  portion: string; // Временно так
   measurement: string;
   createdAt: string;
   updatedAt: string;
@@ -31,10 +44,21 @@ const testRules = {
   required: "Надо заполнить",
 };
 
-export const MeasurementModal = ({ open, handleClose }: MeasurementModal) => {
-  const [measurementType, setMeasurementType] = useState<string>("");
+const modalContentStyles = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 444,
+  bgcolor: "#151b23",
+  padding: "15px",
+  borderRadius: "5px",
+  textAlign: "center",
+};
 
+export const MeasurementModal = ({ open, handleClose }: MeasurementModal) => {
   const dispatch = useAppDispatch();
+  const [measurementType, setMeasurementType] = useState<string>("");
 
   useEffect(() => {
     dispatch(recieveTypesOfMeasurements());
@@ -47,24 +71,18 @@ export const MeasurementModal = ({ open, handleClose }: MeasurementModal) => {
   const typesOptions = [...typeOfMeasurementsState.typesOfMeasurements];
   console.log("typesOptions", typesOptions);
 
-  const modalContentStyles = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 444,
-    bgcolor: "#151b23",
-    padding: "15px",
-    borderRadius: "5px",
-    textAlign: "center",
-  };
-
-  const handleTypeOfMeasurementChange = (value: string) => {
+  const handleTypeOfMeasurementChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = event.target;
+    console.log("value", value);
     if (!value) return;
 
-    const type = typesOptions.filter((item) => item.label === value);
-    if (type.length > 0) {
-      setValue("typeOfMeasurement", type[0].name);
+    setMeasurementType(value);
+
+    const typeId = typesOptions.filter((item) => item.name === value);
+    if (typeId.length > 0) {
+      setValue("typeOfMeasurement", typeId[0].name);
     }
   };
 
@@ -75,39 +93,21 @@ export const MeasurementModal = ({ open, handleClose }: MeasurementModal) => {
     reset,
     formState: { errors },
     clearErrors,
-  } = useForm<FormTypes>({
-    // defaultValues: {
-    //   typeOfMeasurement: "", // Задаем начальное значение для поля
-    //   food: "", // Если это поле тоже существует в форме
-    //   measurement: "",
-    //   createdAt: "",
-    //   updatedAt: "",
-    // },
-  });
+    trigger,
+  } = useForm<FormTypes>();
+
+  console.log("errors", errors);
 
   const resetValues = () => {
     reset();
     clearErrors();
+    setMeasurementType("");
   };
 
   const onSubmit = (formData: FormTypes) => {
     console.log("formData", formData);
     resetValues();
   };
-
-  // <TextField
-  //   id="outlined-select-currency"
-  //   select
-  //   label="Select"
-  //   defaultValue="EUR"
-  //   helperText="Please select your currency"
-  // >
-  //   {currencies.map((option) => (
-  //     <MenuItem key={option.value} value={option.value}>
-  //       {option.label}
-  //     </MenuItem>
-  //   ))}
-  // </TextField>;
 
   return (
     <Modal
@@ -134,33 +134,39 @@ export const MeasurementModal = ({ open, handleClose }: MeasurementModal) => {
             Add new measurement
           </Typography>
           <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              name="typeOfMeasurement"
-              control={control}
-              rules={testRules}
-              render={() => (
-                <TextField
-                  required
-                  select
-                  error={errors.typeOfMeasurement ? true : false}
-                  // onChange={}
-                  // value={}
-                  helperText={
-                    errors.typeOfMeasurement
-                      ? errors.typeOfMeasurement.message
-                      : ""
-                  }
-                  label="Type of measurement"
-                  variant="outlined"
-                >
-                  {typesOptions.map((option) => (
-                    <MenuItem key={option.id} value={option.label}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              )}
-            />
+            <FormControl fullWidth>
+              <Controller
+                name="typeOfMeasurement"
+                control={control}
+                rules={testRules}
+                render={() => (
+                  <TextField
+                    select
+                    error={errors.typeOfMeasurement ? true : false}
+                    onChange={handleTypeOfMeasurementChange}
+                    value={measurementType}
+                    helperText={
+                      errors.typeOfMeasurement
+                        ? errors.typeOfMeasurement.message
+                        : ""
+                    }
+                    label="Type of measurement"
+                    variant="outlined"
+                  >
+                    {typesOptions.map((option) => (
+                      <MenuItem
+                        key={option.id}
+                        value={option.name}
+                        onClick={() => clearErrors("typeOfMeasurement")}
+                      >
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                )}
+              />
+            </FormControl>
+
             <Button type="submit" variant="contained">
               submit
             </Button>
